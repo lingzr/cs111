3192f25b41125ed017eeb5c25e807df935cf4242
fumu <332853786@qq.com>
2016-04-25 12:09:38 -0700

    asdsad

--- proj4/lab2a.c
+++ proj4/lab2a.c
@@ -21,6 +21,7 @@ void add(long long *pointer, long long value)
     switch (sync)
     {
       case 'm':
+        printf("wtf!!!!!!\n");
         pthread_mutex_lock(&count_mutex);
         sum = *pointer + value;
         if (opt_yield)
