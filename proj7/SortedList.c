9fa08a6f3a5f855c926aefe3f55389c0044fc813
fumu <332853786@qq.com>
2016-05-08 17:30:31 -0700

    asdsad

--- proj7/SortedList.c
+++ proj7/SortedList.c
@@ -25,12 +25,6 @@ void SortedList_insert(SortedList_t *list, SortedListElement_t *element)
 
 int SortedList_delete( SortedListElement_t *element)
 {
-	// if (opt_yield & DELETE_YIELD)
-	// 	pthread_yield();
-	// if (element->next->prev != element || element->prev->next != element)
-	// 	return 1;
-	// element->next->prev = element->prev;
-	// element->prev->next = element->next;
 	if (opt_yield & DELETE_YIELD)
 		pthread_yield();
 
@@ -39,19 +33,22 @@ int SortedList_delete( SortedListElement_t *element)
 	element->prev = element;
 	element->next = element;
 
-
 }
 
 SortedListElement_t *SortedList_lookup(SortedList_t *list, const char *key)
 {
-	SortedListElement_t *listptr = list;
 	if (opt_yield & SEARCH_YIELD)
 		pthread_yield();
-	while (listptr->next != list && strcmp(listptr->next->key, key))
-		listptr = listptr->next;
-	if (listptr->next == list)
-		return NULL;
-	return listptr->next;
+
+
+	SortedList_t* current = list;
+	//should insert after the current node
+	while (current->next != list && strcmp(current->next->key,element->key)!=0 )
+		current = current->next;
+
+	return current->next;
+
+
 }
 
 int SortedList_length(SortedList_t *list)
