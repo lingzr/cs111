59f8b7787fb2d157f726ce569463a46d1ec68ad7
fumu <332853786@qq.com>
2016-05-17 01:47:10 -0700

    better

--- labca/testLab3a.c
+++ labca/testLab3a.c
@@ -309,7 +309,7 @@ int print_directory_entry(void* directory_entry_Buffer, FILE* directoryStream)
 		for (k; k<name_size; k++)
 		{
 			name[k]=*(ptr_1+8+k);
-			//printf("%c\n",name[k] );
+			printf("%c\n",name[0] );
 		}
 		//printf("%d\n",name_size );
 
