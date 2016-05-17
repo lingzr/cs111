a34910ed0b0f307b2088c57dcb14882723c894af
fumu <332853786@qq.com>
2016-05-17 04:57:38 -0700

    better

--- labca/testLab3a.c
+++ labca/testLab3a.c
@@ -320,10 +320,10 @@ int print_directory_entry(void* directory_entry_Buffer, FILE* directoryStream)
 		//printf("%d\n",name_size );
 		name[name_size]='\0';
 
-		//if (*(uint32_t*)(ptr_1)!=0)
+		if (*(uint32_t*)(ptr_1)!=0)
 		{
 
-			fprintf(directoryStream, "%d %d %d %d %d %s\n",
+			fprintf(directoryStream, "%d %d %d %d %d \"%s\"\n",
 					1,
 					entryNumber,
 					*(uint16_t*)(ptr_1+4),
